b772fc0017df6aa46d877ebdc0115735c33e7434
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-14 13:20:35 +0200

    Header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
-
+import { Bars3Icon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="grid grid-cols-3 p-10 items-center">
+        <Bars3Icon className="h-8 w-8 cursor-pointer" />
+        <Link href="/" prefetch={false}>
+          <h1>The MDev Movies</h1>
+        </Link>
+
+        <div>
+          <button
+            className="hidden md:inline bg-slate-900 text-white
+           px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800"
+          >
+            Subscribe Now
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
